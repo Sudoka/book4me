bbb7b5669418c27a10ebd894e5eb4ab13a79390b
tvanicraath <iamrachiit@gmail.com>
2012-09-27 20:16:00 +0530

    improved css

---
+++ home_calendar.php
@@ -0,0 +1,40 @@
+<div id="day">
+<table>
+<tr>
+<td id="day_entry">Fri</td>
+<td id="day_entry">Sat</td>
+<td id="day_entry">Sun</td>
+<td id="day_entry">Mon</td>
+<td id="day_entry">Tue</td>
+<td id="day_entry">Wed</td>
+<td id="day_entry">Thu</td>
+<td id="day_entry">Fri</td>
+<td id="day_entry">Sat</td>
+<td id="day_entry">Sun</td>
+<td id="day_entry">Mon</td>
+</tr>
+</table>
+</div>
+
+<div id="time">
+<table>
+<tr> <td id="time_entry">7:00</td></tr>
+<tr> <td id="time_entry">8:00</td></tr>
+<tr> <td id="time_entry">09:00</td></tr>
+<tr> <td id="time_entry">10:00</td></tr>
+<tr> <td id="time_entry">11:00</td></tr>
+<tr> <td id="time_entry">12:00</td></tr>
+<tr> <td id="time_entry">13:00</td></tr>
+<tr> <td id="time_entry">14:00</td></tr>
+<tr> <td id="time_entry">15:00</td></tr>
+<tr> <td id="time_entry">16:00</td></tr>
+<tr> <td id="time_entry">17:00</td></tr>
+<tr> <td id="time_entry">18:00</td></tr>
+<tr> <td id="time_entry">19:00</td></tr>
+<tr> <td id="time_entry">20:00</td></tr>
+<tr> <td id="time_entry">21:00</td></tr>
+<tr> <td id="time_entry">22:00</td></tr>
+<tr> <td id="time_entry">23:00</td></tr>
+</table>
+</div>
+
---
+++ main.php
@@ -0,0 +1,94 @@
+
+<html>
+<head>
+	<title> Event Scheduler </title>
+	<link href="http://www.google.com/s2/favicons?domain=www.google.com/calendar/render?pli=1" type="image/gif" rel="icon" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Nunito:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Relex' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lustria' rel='stylesheet' type='text/css'>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+	<script type="text/javascript" src="ui.js"></script>
+	
+	<div id="header">
+		
+		<div id="header-text">
+			<a class="anchorLink" href="#about-us"> About Us </a>
+		</div>
+		<div id="header-text">
+			<a class="anchorLink" href="#contacts"> Contacts </a>
+		</div>
+		<div id="header-text">
+			<a class="anchorLink" href="#home"> Home </a>
+		</div>
+	</div>
+	</hr>
+	
+	<body>
+	
+		<section id="home">
+		<div id="homeText" class="scrollInfo">
+			<div id="htext">
+		<h1> Home </h1></div>
+		
+		<div id="ptext">
+		<?php include("home_calendar.php"); ?>
+		<!--<p>
+		A home is a place of residence or refuge.
+		[1] When it refers to a building, it is usually a <br>place in which an individual 
+		or a family can live and store personal property. <br>Most modern-day households contain sanitary facilities and<br> a means of preparing food. 
+		Animals have their own homes as well, either living in the <br>wild or shared with humans in a domesticated environment.<br> 
+		"Home" is also used to refer to the geographical area <br>(whether it be a suburb, town, city or country) in which a person grew up <br>or 
+		feels they belong, or it can refer to the native <br>habitat of a wild animal. Sometimes, as an alternative to the <br>
+		definition of "home" as a physical locale<br> ("Home is where you hang your hat"), home may be perceived <br>to have no physical location, 
+		instead, home may relate instead to a<br> mental or emotional state of refuge or comfort. <br>
+		Popular sayings along these lines are <br>"Home is where the heart is" or "You can never go home again".<br>
+		There are cultures in which homes are mobile such as nomadic peoples.<br>
+		</p>-->
+		</div>
+		</div>
+		</section>
+		<section id="contacts">
+		<div id="contactsText" class="scrollInfo">
+		<div id="htext">
+		<h1> Contacts </h1></div>
+		<div id="ptext">
+		<p>A home is a place of residence or refuge.
+		[1] When it refers to a building, it is usually a <br>place in which an individual 
+		or a family can live and store personal property. <br>Most modern-day households contain sanitary facilities and<br> a means of preparing food. 
+		Animals have their own homes as well, either living in the <br>wild or shared with humans in a domesticated environment.<br> 
+		"Home" is also used to refer to the geographical area <br>(whether it be a suburb, town, city or country) in which a person grew up <br>or 
+		feels they belong, or it can refer to the native <br>habitat of a wild animal. Sometimes, as an alternative to the <br>
+		definition of "home" as a physical locale<br> ("Home is where you hang your hat"), home may be perceived <br>to have no physical location, 
+		instead, home may relate instead to a<br> mental or emotional state of refuge or comfort. <br>
+		Popular sayings along these lines are <br>"Home is where the heart is" or "You can never go home again".<br>
+		There are cultures in which homes are mobile such as nomadic peoples.<br>
+		</p>
+		</div>
+		</div>
+		</div>
+		</section>
+		<section id="about-us">
+		<div id="aboutText" class="scrollInfo">	
+		<div id="htext">
+		<h1> About Us </h1></div>
+		<div id="ptext">
+		<p>
+		A home is a place of residence or refuge.
+		[1] When it refers to a building, it is usually a <br>place in which an individual 
+		or a family can live and store personal property. <br>Most modern-day households contain sanitary facilities and<br> a means of preparing food. 
+		Animals have their own homes as well, either living in the <br>wild or shared with humans in a domesticated environment.<br> 
+		"Home" is also used to refer to the geographical area <br>(whether it be a suburb, town, city or country) in which a person grew up <br>or 
+		feels they belong, or it can refer to the native <br>habitat of a wild animal. Sometimes, as an alternative to the <br>
+		definition of "home" as a physical locale<br> ("Home is where you hang your hat"), home may be perceived <br>to have no physical location, 
+		instead, home may relate instead to a<br> mental or emotional state of refuge or comfort. <br>
+		Popular sayings along these lines are <br>"Home is where the heart is" or "You can never go home again".<br>
+		There are cultures in which homes are mobile such as nomadic peoples.<br>
+		</p>
+		</div>
+		</div>
+		</div>
+		</section>
+	</body>
+</head>
+</html>
